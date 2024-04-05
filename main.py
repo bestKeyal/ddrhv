2cef73fb2477388c5fa53ca8982f3921c418b0a3
Keyal <1920194156@qq.com>
2024-04-05 09:11:28 +0800

    t

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ data_gen_args = dict(
 if __name__ == '__main__':
     #############################################Training Parameters#######################################################
     num_CV = 1
-    NumEpochs = 1
+    NumEpochs = 20
     NumEpochEval = 1  # validated the model each NumEpochEval epochs
     batch_size = 64
     learning_rateI = 1e-5
